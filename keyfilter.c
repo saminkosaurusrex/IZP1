837666ed2b6b921a09232da0ce30e270f51a47c8
saminkosaurusrex <Jablonov1@>
2023-10-06 21:49:56 +0200

    skuska

---
+++ keyfilter.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+//void scanFile(char letter, char array[1][1]);
+
+void getFirstLetter();
+int isIn(int arraySize, char array[], char letter);
+
+int main(int argc, char *argv[]) {
+    int letter, i;
+    char letterArray[1000] = {0};
+
+
+   // printf("%d", argc);
+    if (argc == 1)
+    {
+        getFirstLetter();
+                
+    }
+
+
+    return 0;
+}
+
+void getFirstLetter(){
+    int letter;
+    int i = 0;
+    //int isIn = 0;
+    char letterArray[1000] = {0};
+    while ((letter = getchar()) != EOF)
+    {
+        if (letter == '\n' || i == 0)
+        {
+            if (i == 0)
+            {
+                letterArray[i] = letter;
+                printf("%c\n", letterArray[i]);
+                i++;
+            }else{
+                char pismenko = getchar();
+                //printf("%d %c %d", isIn(i, letterArray, pismenko), pismenko, i);
+                if (isIn(i, letterArray, pismenko) != 1)
+                {
+                    letterArray[i] = pismenko;
+                    printf("%c\n", letterArray[i]);
+                    i++;
+                    
+                }
+                
+                //scanf("%s", &letterArray[i]);
+                //printf("%c\n", letterArray[i]);
+
+            }
+        }
+    }
+}
+
+int isIn(int arraySize, char array[], char letter){//2 krat 
+    int isIn = 0;
+    for (int i = 0; i < arraySize; i++)
+    {
+        if (letter == array[i])
+        {
+            isIn = 1;
+        }
+        else{
+            isIn = 0;
+        }
+    }
+    return isIn;
+}
+
+
+
+
+
